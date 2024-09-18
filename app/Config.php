2636b4c7ef440cb92e9cae74328d44da042b4475
Ivan Polák <ivan.polak25@gmail.com>
2024-09-18 19:45:08 +0200

    update db location

--- app/Config.php
+++ app/Config.php
@@ -4,9 +4,8 @@ namespace App;
 
 class Config
 {
-    const PATH_TO_SQLITE_DB = '/Users/ivanpolak/Code/_software-halova/sql/database.sqlite';
+    const PATH_TO_SQLITE_DB = 'database.sqlite';
 
     const URL = 'http://localhost:5001';
-
 
 }
